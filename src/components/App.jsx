e26d825e2c35ecbf87a2cafffd1645d12a5134d5
camariu <amariutei1998@gmail.com>
2023-08-25 13:41:43 +0300

    hw-01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
+import { useState } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { nanoid } from 'nanoid';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+
+  const [filter, setFilter] = useState('');
+
+  const addContacts = (name, number) => {
+    const newContact = { id: nanoid(), name, number };
+    setContacts([...contacts, newContact]);
+  };
+
+  const handeleFilterChange = value => {
+    setFilter(value);
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  const handleDeleteContact = id => {
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updatedContacts);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1
+        style={{
+          marginLeft: 10,
+        }}
+      >
+        Phonebook
+      </h1>
+      <ContactForm onAddContact={addContacts} />
+      <h2
+        style={{
+          marginLeft: 10,
+          marginTop: 15,
+        }}
+      >
+        Contacts
+      </h2>
+      <Filter onFilterChange={handeleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContacts={handleDeleteContact}
+      />
     </div>
   );
 };
